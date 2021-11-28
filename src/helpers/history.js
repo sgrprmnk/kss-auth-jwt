dd6b89c1e02e2f55f323f882496793fa575064b1
Sagar Pramanik <sgrprmnk@gmail.com>
2021-11-28 13:29:05 +0530

    Add files via upload

---
+++ src/helpers/history.js
@@ -0,0 +1,11 @@
+import { createBrowserHistory } from "history";
+
+export const history = createBrowserHistory();
+
+// import React from "react";
+
+// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+// import { useHistory } from "react-router-dom";
+
+// export const history = useHistory();
